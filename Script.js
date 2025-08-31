d13af0923eec652024269addfdbbd31816c9a42b
Kennycoder18 <201223017+Kennycoder18@users.noreply.github.com>
2025-08-31 03:20:41 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -19,3 +19,21 @@ document.getElementById("Top")
     }
 
 );
+
+//scroll-based animation effect
+// const sections = document.querySelectorAll('div');
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animate');
+//             observer.unobserve(entry.target); // Stop observing once animated
+//         }
+//     });
+// }, {
+//     threshold: 0.5 // Trigger when 50% of the element is visible
+// });
+
+// sections.forEach(section => {
+//     observer.observe(section);
+// });
